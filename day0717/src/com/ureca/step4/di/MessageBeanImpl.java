a5359dee5cdc0c03d29bac373f3f3f0bf746e37c
dldpdnjs21 <2dpdnjs@naver.com>
2024-07-17 13:19:09 +0900

    FileOutput

--- day0717/src/com/ureca/step4/di/MessageBeanImpl.java
+++ day0717/src/com/ureca/step4/di/MessageBeanImpl.java
@@ -5,6 +5,11 @@ public class MessageBeanImpl implements MessageBean {
 	//private 오직 해당 클래스에서만 사용가능(자식 클래스도 사용X)
 	private String greeting;
 	private String name;
+	private FileOutput fo;
+	
+	public void setFo(FileOutput fo) {
+		this.fo = fo;
+	}
 	
 //	public 필드의 자료형 get+필드명(){return 데이터;}
 //	public void set+필드명(필드연관된 변수선언){this.필드=변수;}
